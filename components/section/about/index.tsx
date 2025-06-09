111f809b762fa9b4aacf7c8c06364c7ccbd59853
dwi-mac <dwipascanursanti020@gmail.com>
2025-06-09 20:42:10 +0700

    chore: refactor about page

---
+++ components/section/about/index.tsx
@@ -0,0 +1,5 @@
+export { default as HeroSection } from './HeroSection';
+export { default as AboutSection } from './AboutSection';
+export { default as VisiMisiSection } from './VisiMisiSection';
+export { default as ManagementSection } from './ManagementSection';
+export { default as ContactInformationSection } from './ContactInformationSection';

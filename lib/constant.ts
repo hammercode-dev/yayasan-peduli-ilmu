c7844157b3dcba2d1157a692b32ed6ad668e89b9
dwi-mac <dwipascanursanti020@gmail.com>
2025-07-18 18:33:33 +0700

    fix: navlink in navbar and footer

--- lib/constant.ts
+++ lib/constant.ts
@@ -12,3 +12,20 @@ export const BANK_ACCOUNTS = [
     swift: 'BSMDIDJAXXX',
   },
 ];
+
+export const NAV_LINKS = [
+  { href: '/', label: 'home' },
+  { href: '/about', label: 'about' },
+  { href: '/contact', label: 'contact' },
+  // { href: '/report', label: 'reports' },
+];
+
+export const PROGRAM_LINKS = [
+  { href: '/program/pondok-pesantren', label: 'cottage', desc: 'Putra & Putri' },
+  // { href: '/program/komunitas-tashil', label: 'community', desc: 'Palu' },
+];
+
+export const PUBLICATION_LINKS = [
+  { href: '/publications/article', label: 'article' },
+  { href: '/publications/book', label: 'book' },
+];
